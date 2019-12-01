95a2334da577f7e69cdb9b04ae46fedcdbe7734b
myusername38 <martin38@live.unc.edu>
2019-12-01 15:25:59 -0500

    animations removed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { redirectUnauthorizedTo, AngularFireAuthGuard, canActivate } from '@angular/fire/auth-guard';
-import { LoadPageComponent } from './components/load-page/load-page.component';
+import { HomeComponent } from './components/home/home.component'
+import { redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 
 const redirectUnauthorizedToLanding = redirectUnauthorizedTo(['home']);
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent, data: { animation: 'isRight' } },
-  { path: 'reset', component: ResetPasswordComponent, data: { animation: 'isRight' } },
-  { path: 'register', component: RegisterComponent, data: { animation: 'isLeft' } },
-  { path: 'about', component: AboutComponent, data: { animation: 'isRight' } },
+  { path: 'login', component: LoginComponent },
+  { path: 'reset', component: ResetPasswordComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'editor', loadChildren: () => import ('./code-editor/code-editor.module').then(mod => mod.CodeEditorModule) },
   { path: '**', redirectTo: '' },
 ];
